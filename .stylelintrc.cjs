aa16f9c396552654c12dbbf362a34956a431fae7
jhoonyeel <jhoonyeel@gmail.com>
2025-06-23 13:34:11 +0900

    chore: stylint-order 플러그인 기반 preset extends로 변경
    stylelint-config-rational-order이 더 큰 집합
    
    KB3-47

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,51 +1,14 @@
 // .stylelintrc.cjs
 module.exports = {
-  processors: ["stylelint-processor-styled-components"],
+  customSyntax: 'postcss-styled-syntax',
   extends: [
     'stylelint-config-recommended', // 최소 설정
     'stylelint-config-styled-components', // styled-components 호환성
     'stylelint-config-standard', // 표준 CSS 룰
+    'stylelint-config-rational-order', // CSS 속성 자동 정렬
     'stylelint-config-prettier', // Prettier와 충돌 방지
   ],
-  plugins: ['stylelint-order'],
   rules: {
-    'order/properties-order': [
-      [
-        // ✅ 위치 관련 우선
-        'position',
-        'top',
-        'left',
-        'bottom',
-        'right',
-        'z-index',
-
-        // ✅ 박스 모델 관련
-        'display',
-        'flex-direction',
-        'align-items',
-        'justify-content',
-
-        // ✅ 마진/패딩
-        'margin',
-        'padding',
-
-        // ✅ 기타 시각 속성
-        'width',
-        'height',
-        'font-size',
-        'font-weight',
-        'line-height',
-
-        // ✅ 마지막으로 색/배경 등 디자인
-        'color',
-        'background-color',
-        'border',
-        'box-shadow',
-      ],
-      {
-        unspecified: 'bottomAlphabetical',
-      },
-    ],
     'no-empty-source': null, // 스타일 코드를 포함한 JS/TSX 파일만 검사
   },
 };
