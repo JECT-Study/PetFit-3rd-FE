a855f5da47eca63ed84a11da88a04c493dcb53f9
jhoonyeel <jhoonyeel@gmail.com>
2025-06-23 13:15:10 +0900

    chore: stylelint 호환성 고려해 v14로 마이그레이션

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,18 +1,22 @@
 // .stylelintrc.cjs
-/**
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  processors: ["stylelint-processor-styled-components"],
+  extends: [
+    'stylelint-config-recommended', // 최소 설정
+    'stylelint-config-styled-components', // styled-components 호환성
+    'stylelint-config-standard', // 표준 CSS 룰
+    'stylelint-config-prettier', // Prettier와 충돌 방지
+  ],
   plugins: ['stylelint-order'],
-  customSyntax: 'postcss-styled-syntax',
   rules: {
     'order/properties-order': [
       [
         // ✅ 위치 관련 우선
         'position',
         'top',
-        'right',
-        'bottom',
         'left',
+        'bottom',
+        'right',
         'z-index',
 
         // ✅ 박스 모델 관련
@@ -29,6 +33,7 @@ module.exports = {
         'width',
         'height',
         'font-size',
+        'font-weight',
         'line-height',
 
         // ✅ 마지막으로 색/배경 등 디자인
@@ -38,9 +43,9 @@ module.exports = {
         'box-shadow',
       ],
       {
-        unspecified: 'bottomAlphabetical', // 명시 안된 속성은 마지막에 알파벳 순
+        unspecified: 'bottomAlphabetical',
       },
     ],
+    'no-empty-source': null, // 스타일 코드를 포함한 JS/TSX 파일만 검사
   },
 };
- */
