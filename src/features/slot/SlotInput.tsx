74fdc1fda7c507923c070651ffa1fe21e1e85b0a
허세령 <seryoii@naver.com>
2025-07-02 06:42:39 +0900

    feat: 슬롯 설정 시 입력창 UI 구현

---
+++ src/features/slot/SlotInput.tsx
@@ -0,0 +1,51 @@
+import { Menu } from 'lucide-react';
+import styled from 'styled-components';
+
+import { SLOT_ITEMS } from '@/constants/slot';
+
+export const SlotInput = () => {
+  return (
+    <div>
+      {SLOT_ITEMS.map(({ id, icon, label }) => (
+        <InputContainer key={id}>
+          <InputHeader>
+            <img src={icon} alt={label} /> {label}
+          </InputHeader>
+          <InputContent>
+            <Input />
+            <Menu color={'#A5A5A5'} />
+          </InputContent>
+        </InputContainer>
+      ))}
+    </div>
+  );
+};
+
+const InputContainer = styled.div`
+  border-bottom: 1px solid #e0e0e0;
+  margin: 10px 0;
+`;
+
+const InputHeader = styled.div`
+  display: flex;
+  gap: 5px;
+  font-size: 12px;
+`;
+
+const InputContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Input = styled.input`
+  width: calc(100%);
+  height: 45px;
+  border-radius: 8px;
+  border-width: 1px;
+  padding: 12px 20px;
+  margin: 12px 0px;
+  background: #fff8e5;
+  border: 1px solid #dddddd;
+  font-size: 14px;
+`;
--- src/pages/SlotSettingPage.tsx
+++ src/pages/SlotSettingPage.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
+import styled from 'styled-components';
+
 import { SlotButton } from '@/features/slot/SlotButton';
+import { SlotInput } from '@/features/slot/SlotInput';
 
 export const SlotSettingPage = () => {
   const [selectedIds, setSelectedIds] = useState<string[]>([]);
@@ -9,8 +12,20 @@ export const SlotSettingPage = () => {
     setSelectedIds(prev => (prev.includes(id) ? prev.filter(k => k !== id) : [...prev, id]));
 
   return (
-    <div>
+    <Wrapper>
       <SlotButton selectedIds={selectedIds} onToggle={toggle} />
-    </div>
+      <RoutineTitle>활성화된 루틴</RoutineTitle>
+      <SlotInput />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 24px 16px;
+`;
+
+const RoutineTitle = styled.div`
+  margin: 30px 0;
+  font-size: 18px;
+  font-weight: 600;
+`;
