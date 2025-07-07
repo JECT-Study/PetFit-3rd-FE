a13277ae22c57a85bb6c6f2caee04fb923f5fed0
jhoonyeel <jhoonyeel@gmail.com>
2025-07-08 06:52:50 +0900

    feat: 홈화면에 사용되는 모킹 데이터 분리
    KB3-63

---
+++ src/mocks/homeData.ts
@@ -0,0 +1,10 @@
+export const nameListMock = [
+  { name: '대표이름', isMain: true },
+  { name: '이름1', isMain: false },
+  { name: '이름2', isMain: false },
+  { name: '이름3', isMain: false },
+  { name: '이름4', isMain: false },
+];
+
+export const scheduleMock = ['일정제목 최대 1줄 노출', '일정제목 최대 1줄 노출'];
+export const noticeMock = ['특이사항제목 최대 1줄 노출', '특이사항제목 최대 1줄 노출'];
