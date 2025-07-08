38242b1e6562429cd2cc51aa0f34ff1c51a535d5
허세령 <seryoii@naver.com>
2025-07-09 07:20:36 +0900

    feat: 마이페이지 목록 ui 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,52 @@
-const MyPage = () => {
-  return <div>MyPage</div>;
+import { ChevronRight } from 'lucide-react';
+import styled from 'styled-components';
+
+import { Profile } from '@/features/mypage/Profile';
+
+export const MyPage = () => {
+  return (
+    <div>
+      <Profile />
+      <MenuContainer>
+        <MenuTitle>반려동물 설정</MenuTitle>
+        <Menu>
+          반려동물 정보 관리 <ChevronRight size={20} />
+        </Menu>
+
+        <MenuTitle>기타</MenuTitle>
+        <Menu>버전 정보</Menu>
+        <Menu>
+          로그아웃 <ChevronRight size={20} />
+        </Menu>
+        <Menu>
+          <DeleteButton>탈퇴하기</DeleteButton>
+          <ChevronRight size={20} />
+        </Menu>
+      </MenuContainer>
+    </div>
+  );
 };
 
-export default MyPage;
+const MenuContainer = styled.div`
+  margin-top: 20px;
+`;
+const MenuTitle = styled.div`
+  padding: 12px 16px;
+  font-size: 12px;
+  font-weight: 500;
+  color: #666666;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  font-size: 13px;
+  font-weight: 500;
+  border-bottom: 1px #f0f0f0 solid;
+`;
+
+const DeleteButton = styled.div`
+  color: #ff5c33;
+`;
