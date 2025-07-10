783b30d6255df3bf2950e29aceda230e50334962
허세령 <seryoii@naver.com>
2025-07-11 04:49:48 +0900

    feat: axiosInstace, auth 관련 api, 로그인 버튼 클릭 시 redirect URI,  gitignore .env 추가

---
+++ src/apis/auth.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+import { axiosInstance } from './axiosInstance';
+
+export const kakaoLogin = async (code: string) => {
+  try {
+    await axios.get(`auth/kakao/login`, {
+      params: { code },
+    });
+  } catch (error) {
+    console.error('kakao Login failed:', error);
+    throw error;
+  }
+};
+
+export const kakaoLoginDev = async (code: string) => {
+  try {
+    await axios.get(`auth/kakao/login/dev`, {
+      params: { code },
+    });
+  } catch (error) {
+    console.error('kakao Login failed:', error);
+    throw error;
+  }
+};
+
+export const kakaoLogout = async () => {
+  try {
+    await axiosInstance.post('auth/kakao/logout', '');
+  } catch (error) {
+    console.error('logout failed: ', error);
+    throw error;
+  }
+};
+
+export const kakaoLogoutDev = async () => {
+  try {
+    await axiosInstance.post('auth/kakao/logout/dev', '');
+  } catch (error) {
+    console.error('logout failed: ', error);
+    throw error;
+  }
+};
+
+export const kakaoWithdraw = async () => {
+  try {
+    await axiosInstance.post('auth/kakao/withdraw');
+  } catch (error) {
+    console.error('user delete failed: ', error);
+    throw error;
+  }
+};
